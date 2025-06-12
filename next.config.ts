5084a030c6e8664a1bd7bf03c7d83f0222a8aef7
ryongseong <ryongseong.dev@gmail.com>
2025-06-12 16:52:04 +0900

    Fix : 오류 수정?

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -21,6 +22,10 @@ const nextConfig: NextConfig = {
         hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
   },
 };
 
