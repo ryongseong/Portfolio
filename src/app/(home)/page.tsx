c9d1fb573110966dcc7107805c69785b3dbf2ca1
ryongseong <ryongseong.dev@gmail.com>
2025-06-12 17:42:21 +0900

    Feat : 가장 상단으로 이동하는 버튼 생성

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/home/about";
 import AboutMe from "@/components/home/AboutMe";
+import ScrollToTopBtn from "@/components/home/ScrollToTopBtn";
 import Skills from "@/components/home/skills";
 
 export default function Home() {
@@ -19,6 +20,9 @@ export default function Home() {
         className="container mx-auto flex px-6 md:flex-row flex-col items-center justify-center mt-36"
       >
         <Skills />
+      </div>
+      <div>
+        <ScrollToTopBtn />
       </div>
     </section>
   );
---
+++ src/components/home/ScrollToTopBtn.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+
+export default function ScrollToTopBtn() {
+  return (
+    <Link
+      href="/"
+      className="fixed bottom-20 right-4 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-gray-800 text-white shadow-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
+        />
+      </svg>
+    </Link>
+  );
+}
