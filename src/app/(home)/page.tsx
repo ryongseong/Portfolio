602351babe41dbb7a21ceb4fa959a0db6ef3e111
ryongseong <ryongseong.dev@gmail.com>
2025-06-12 16:33:57 +0900

    Fix : 에러 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,6 @@
-import About from "@/components/home/About";
-import AboutMe from "@/components/home/AboutMe";
-import Skills from "@/components/home/Skills";
+import About from "@/components/home/about";
+import AboutMe from "@/components/home/aboutMe";
+import Skills from "@/components/home/skills";
 
 export default function Home() {
   return (
