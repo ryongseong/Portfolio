c3ce4a4b58bc09b87c4d9105f9463de8d4683549
ryongseong <ryongseong.dev@gmail.com>
2025-10-15 16:57:37 +0900

    style: 메인 페이지 스크롤 버튼 애니메이션 추가

--- src/components/home/ScollToBtn.tsx
+++ src/components/home/ScollToBtn.tsx
@@ -3,6 +3,7 @@
 export default function ScrollToBtn({ target }: { target: string }) {
   return (
     <button
+      className="mt-10 mb-10 animate-bounce rounded-full p-3  focus:outline-none"
       onClick={() => {
         const el = document.getElementById(target);
         if (el) {
