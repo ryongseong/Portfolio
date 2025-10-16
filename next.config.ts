8ab6290e63d0bad7a3b9833994c0d217fa37c047
ryongseong <ryongseong.dev@gmail.com>
2025-10-16 10:06:59 +0900

    fix: 프로젝트 배너 로딩 시 불러오지 못하는 문제 해결

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,8 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
