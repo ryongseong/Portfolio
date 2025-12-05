ba57476f68e65dc970db3e80835b4ff9202c9919
ryongseong <ryongseong.dev@gmail.com>
2025-12-05 10:34:03 +0900

    fix: 컴포넌트 임포트 문제 해결

--- src/components/home/Experience.tsx
+++ src/components/home/Experience.tsx
@@ -1,5 +1,5 @@
 import { experiences } from "@/data/experiences";
-import ExperienceCard from "./experienceCard";
+import ExperienceCard from "./ExperienceCard";
 
 export default function Experience() {
   return (
